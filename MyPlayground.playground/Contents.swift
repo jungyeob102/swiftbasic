542ebd370b832e3c6d512091d741936d256b3d79
JUNGYEOBLEE <asdf95102@naver.com>
2021-01-16 21:01:46 +0900

    08. 함수 고급

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -258,7 +258,7 @@ let subtracting: Set<Int> = setA.subtracting(setB) //subtracting 차집합
 //MARK: - 함수의 선언
 
 //MARK: 함수 선언의 기본형태
-//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 ...) -> 반환타입 {
+//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입) -> 반환타입 {
 //   함수 구현부
 //   return 반환값
 //}
@@ -269,7 +269,7 @@ func sum(a: Int, b: Int) -> Int{
 }
 
 //MARK: 반환 값이 없는 함수
-//func 함수이름(매개변수1이르미 매개변수1타입, 매개변수2이름: 매개변수2타입 ...) -> Void {     << Void 생략도 가능
+//func 함수이름(매개변수1이르미 매개변수1타입, 매개변수2이름: 매개변수2타입) -> Void {     << Void 생략도 가능
 //   함수 구현부
 //   return
 //}
@@ -304,7 +304,7 @@ printMyName(name: "jungyeob")
 /*
  정리
  함수선언의 기본형태
- func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 ...) -> 반환타입{
+ func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입) -> 반환타입{
      함수 구현부
      return 반환값
  }
@@ -317,3 +317,52 @@ func multiplication(a: Int, b: Int) -> Int{
 
 multiplication(a: 5, b: 10)
 
+// 08. 함수 고급----------------------------------------------------------------------------------------------------------------
+
+//MARK: - 매개변수 기본값
+//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 = 매개변수 기본값) -> 반환타입{
+//   함수 구현부
+//   return 반환값
+
+func greeting(friend: String, me : String = "jungyeob") {
+    print("Hello \(friend)! I'm \(me)")
+}
+
+greeting(friend: "yeob")
+
+//MARK: - 전달인자 레이블
+
+/*
+ 전달인자 레이블은 함수를 호출할 때
+ 매개변수의 역할을 좀 더 명확하게 하거나
+ 함수 사용자의 입장에서 표현하고자 할 때 사용한다
+ */
+
+//func 함수이름(전달인자 레이블 매개변수1이름: 매개변수1타입, 전달인자 레이블 매개변수2이름: 매개변수2타입) -> 반환타입{
+//   함수 구현부
+//   return 반환값
+
+func greeting(to friend: String,from me : String = "jungyeob") {
+    print("Hello \(friend)! I'm \(me)")
+}
+
+greeting(to: "yeob")
+
+//MARK: - 가변 매개변수
+
+/*
+전달받을 값의 개수를 알기 어려울 때 사용할 수 있습니다.
+가변 매개변수는 함수당 하나만 가질 수 있습니다.
+ */
+
+//func 함수이름(매개변수1이름: 매개변수1타입,매개변수2이름: 매개변수2타입...) -> 반환타입{
+//   함수 구현부
+//   return 반환값
+
+func sayHelloToFriedns(me: String, friends: String...) -> String{
+    return "Hello \(friends)! I'm \(me)!"
+}
+
+print(sayHelloToFriedns(me: "jungyeob", friends: "hana","mana"))
+
+//타입이 다른 함수는 할당할 수 없다.
