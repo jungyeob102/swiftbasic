296bb515164a08844f9e5ab7c96e6aa34288d3d2
JUNGYEOBLEE <asdf95102@naver.com>
2021-01-18 02:01:43 +0900

    15. 열거형

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -573,3 +573,89 @@ Sample.typeMethod() // type method
 //}
 
 //MARK: 클래스 사용
+
+//MARK: 15강 열거형-------------------------------------------------------------------------------------------------------------
+
+//MARK: - 정의
+
+//enum은 타입으므로 대문자 카멜케이스를 사용하여 이름을 정의한다.
+//각 case는 소문자 카멜케이스로 정의한다.
+//각 case는 그 자체가 고유의 값이다.
+
+// enum 이름 {
+//      case 이름1
+//      case 이름2
+//      case 이름3, 이름4, 이름5
+//      ...
+//}
+
+//MARK: 열거형 사용
+
+enum Weekday{
+    case mon
+    case tue
+    case    wed
+    case    thu, fri, sat, sun
+}
+
+var day: Weekday = Weekday.mon
+day = .fri
+
+
+print(day)
+
+//MARK: 윈시값
+
+//enum은 정수값을 가질 수도 있다.
+//rawBalue를 사용하면 된다.
+//case별로 각기 다른 값을 가져야 한다.
+
+enum Fruit: Int{
+    case apple = 0
+    case grape = 1
+    case peach     //자동으로 1씩 증가한다 즉, peach는 2이다.
+}
+
+print("Fruit.peach.rawValue == \(Fruit.peach.rawValue)")
+
+// 정수 타입뿐만 아니라 Hashable 프로토콜을 따르는 모든 타입이 원시값의 타입으로 지정될 수 있다.
+
+enum School: String {
+    case elementary = "초등"
+    case middle = "중등"
+    case high = "고등"
+    case university  // 숫자 타입이 아닌 문자열은 이름 그대로 가져온다.
+}
+
+print("\(School.high.rawValue)")
+
+//MARK: 원시값을 통한 초기화
+
+//rawValue를 통해 초기화 할 수 있다.
+//rawValue가 case에 해당하지 않을 수 있으므로
+//rawValue를 통해 초기화 한 인스턴스는 옵셔널 타입이다.
+
+let apple : Fruit? = Fruit(rawValue: 0)
+
+if let orange: Fruit = Fruit(rawValue: 5) {
+    print("rawValue 5에 해당하는 케이스는 \(orange)입니다")
+} else {
+    print("rawValue 5에 해당하는 케이스가 없습니다")
+}
+
+//MARK: 메서드
+/*
+enum School: String {
+    case elementary
+    case middle
+    case high
+    case university
+
+    func printMessage()  {
+        switch self {
+        case .elementary:
+            print("초등학생")
+        } 이런식으로 조건을 붙일 수 있음
+    }
+}
+*/
