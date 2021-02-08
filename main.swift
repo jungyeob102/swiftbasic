055366fb5b393310e1addf338ecca93a01140e58
Lee <asdf95102@naver.com>
2021-02-09 02:15:13 +0900

    백준 10998

--- main.swift
+++ main.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-//백준 1001번
-//readLine() 함수는 String? 값으로 입력값을 주기 때문에, 강제언래핑
-let minus = readLine()!.split(separator: " ").map{(A : Substring) -> Int in
+//백준 10718
+
+let multiply =  readLine()!.split(separator: " ").map{(A : Substring) -> Int in
     return Int(A)!
 }
-print(minus[0] - minus [1])
+
+print(multiply[0]*multiply[1])
