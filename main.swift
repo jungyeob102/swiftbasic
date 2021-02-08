06e967a1a910fca398fdd2dff064d52f0ac69d25
Lee <asdf95102@naver.com>
2021-02-09 02:45:33 +0900

    백준 10430

--- main.swift
+++ main.swift
@@ -7,14 +7,18 @@
 
 import Foundation
 
-//백준 10869
+//백준 10430
 
-let Q =  readLine()!.split(separator: " ").map{(A : Substring) -> UInt in
-    return UInt(A)!
+let line = readLine()
+let abc = line!.split(separator: " ").map{(a:Substring) -> Int in
+    return Int(a)!
 }
+let A = abc[0]
+let B = abc[1]
+let C = abc[2]
 
-print(Q[0] + Q[1])
-print(Q[0] - Q[1])
-print(Q[0] * Q[1])
-print(Q[0] / Q[1])
-print(Q[0] % Q[1])
+
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
